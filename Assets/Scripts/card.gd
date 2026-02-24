7ac6fb8758a38da4f80237a15fe8c2889cb8d27e
Jonathan Luc <jonathanluc2010@yahoo.com>
2026-02-23 23:00:53 -0800

    Starting to initialize a scene.

--- Assets/Scripts/card.gd
+++ Assets/Scripts/card.gd
@@ -1,13 +1,19 @@
 extends Area2D
 
-var rank = 13
-@export var debug_name = "Ace of Spades"
+var rank
+var suit
+
+var card_name = "Unnamed Card"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
+func setup(rank: int, suit: String) -> void:
+	rank = rank
+	suit = suit
+	card_name = str(rank, " of ", suit)
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
@@ -16,5 +22,5 @@ func _process(delta: float) -> void:
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == MouseButton.MOUSE_BUTTON_LEFT and event.pressed:
-			print("clicked" + debug_name)
+			print("clicked " + card_name)
 			get_viewport().set_input_as_handled()
