30766ebd2e039c1cf0a82d48608b418804fdf5df
Jonathan Luc <jonathanluc2010@yahoo.com>
2026-03-25 10:52:57 -0700

    formatting code

--- Assets/Scripts/center_pile.gd
+++ Assets/Scripts/center_pile.gd
@@ -24,25 +24,25 @@ func is_pair() -> bool:
 		return false
 	return cards[-1].rank == cards[-2].rank
 
-# start with sandwich & pair
 func is_valid_slap() -> bool:
 	for rule in rules:
 		if rule.call():
 			return true
 	return false
-	
-func _add_two() -> void:
-	var new_card = card_tscn.instantiate()
-	new_card.setup(Card.Suit.SPADES, Card.Rank.TWO)
-	add_card(new_card)
-	add_child(new_card)
-	
+
 func _ready() -> void:
 	# this can be where we change what rules are added to the checklist depending
 	# on player customization.
 	rules.append(is_sandwich)
 	rules.append(is_pair)	
 
-
 func _on_two_button_pressed() -> void:
 	_add_two()
+	
+# for debugging slap rules
+func _add_two() -> void:
+	var new_card = card_tscn.instantiate()
+	new_card.setup(Card.Suit.SPADES, Card.Rank.TWO)
+	add_card(new_card)
+	add_child(new_card)
+	
