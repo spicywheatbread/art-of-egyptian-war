97900fe9b7389739b0afbd60687590c84efcbdd6
Jonathan Luc <jonathanluc2010@yahoo.com>
2026-04-15 10:41:27 -0700

    Starting work on game view model.

--- Assets/Scripts/main_game.gd
+++ Assets/Scripts/main_game.gd
@@ -1,19 +1,36 @@
 extends Node2D
 
-signal game_state (payload: Dictionary)
+var lobby : Dictionary
+@export var player_tscn : PackedScene
+var player_offsets = [
+	Vector2(0, -100),
+	Vector2(0, 100),
+	Vector2(200, 0),
+	Vector2(-200, 0),
+]
 
 func _ready() -> void:
-	pass
+	NetworkClient.game_state.connect(_on_game_state)
+	NetworkClient.lobby_state.connect(_on_lobby_state)
 	
 func _process (delta: float): 
-	var state = await game_state
-	_on_game_state(state) 
+	pass
 
 func _exit_tree() -> void:
 	pass
 
 func _on_game_state (payload: Dictionary):
-	# update game ui here 
+	for key in payload.keys():
+		print("Key: ", key)
+		print("Value: ", payload[key])
+	
+func _on_lobby_state(payload: Dictionary):
+	var l = payload["lobby"]
+	if lobby != l:
+		lobby = l
+		redraw_lobby()
+
+func redraw_lobby():
 	pass
 	
 func _on_play_button_pressed ():
